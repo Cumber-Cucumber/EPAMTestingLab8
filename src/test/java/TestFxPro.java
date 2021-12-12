89e1c82f28e53f07a3d3d444dbb3ecb5aa761f55
Ivan Valui <Wanjawaluj111@gmail.com>
2021-12-12 20:41:09 +0300

    Remove sleep

--- src/test/java/TestFxPro.java
+++ src/test/java/TestFxPro.java
@@ -1,4 +1,6 @@
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -8,36 +10,38 @@ import org.openqa.selenium.WebElement;
 public class TestFxPro {
     @Test
     public void main() throws InterruptedException{
+
         System.setProperty("webdriver.chrome.driver","chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://edge.fxpro.com/");
 
-        Thread.sleep(3000);
-        WebElement searchInput = driver.findElement(By.cssSelector("#container > div > div > div > form > div > input:nth-child(4)"));
+
+        WebElement searchInput = new WebDriverWait(driver,10)
+                .until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#container > div > div > div > form > div > input:nth-child(4)")));
         searchInput.sendKeys("Wanjawaluj111@gmail.com");
         searchInput = driver.findElement(By.cssSelector("#container > div > div > div > form > div > input:nth-child(5)"));
         searchInput.sendKeys("sP22j8pQ_h$hyGy");
-        Thread.sleep(2000);
-        searchInput = driver.findElement(By.cssSelector("#container > div > div > div > form > div > button"));
+        searchInput = new WebDriverWait(driver,10)
+                .until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#container > div > div > div > form > div > button")));
         searchInput.click();
-        Thread.sleep(3000);
-        searchInput = driver.findElement(By.cssSelector("#app > div:nth-child(2) > div > div > div > div.account-select-dialog > div.accounts-to-select-list.desktop-view > div > div.scrollable > div:nth-child(2)"));
+        searchInput = new WebDriverWait(driver,10)
+                .until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#app > div:nth-child(2) > div > div > div > div.account-select-dialog > div.accounts-to-select-list.desktop-view > div > div.scrollable > div:nth-child(2)")));
         searchInput.click();
-        Thread.sleep(3000);
-        searchInput = driver.findElement(By.cssSelector("#app > div.sidebar > section.sidebar__trailing > i.font-icon.icon_settings"));
+        searchInput = new WebDriverWait(driver,10)
+                .until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#app > div.sidebar > section.sidebar__trailing > i.font-icon.icon_settings")));
         searchInput.click();
-        Thread.sleep(2000);
-        searchInput = driver.findElement(By.cssSelector("#app > div.workspace-area > div:nth-child(3) > div > div > div > div:nth-child(2) > div > div > div > div > div > div:nth-child(1)"));
+        searchInput = new WebDriverWait(driver,10)
+                .until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#app > div.workspace-area > div:nth-child(3) > div > div > div > div:nth-child(2) > div > div > div > div > div > div:nth-child(1)")));
         searchInput.click();
-        Thread.sleep(2000);
-        searchInput = driver.findElement(By.cssSelector("#app > div.workspace-area > div:nth-child(3) > div:nth-child(2) > div > div > div:nth-child(2) > div > div > div > div > div > div.scrollable > div:nth-child(1)"));
+        searchInput = new WebDriverWait(driver,10)
+                .until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#app > div.workspace-area > div:nth-child(3) > div:nth-child(2) > div > div > div:nth-child(2) > div > div > div > div > div > div.scrollable > div:nth-child(1)")));
         searchInput.click();
-        Thread.sleep(2000);
 
-        searchInput = driver.findElement(By.cssSelector("#app > div.workspace-area > div:nth-child(3) > div > div > div > div:nth-child(2) > div > div > div > div > div > div:nth-child(1)"));
+        searchInput = new WebDriverWait(driver,10)
+                .until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#app > div.workspace-area > div:nth-child(3) > div > div > div > div:nth-child(2) > div > div > div > div > div > div:nth-child(1)")));
         searchInput.click();
-        Thread.sleep(2000);
-        searchInput = driver.findElement(By.cssSelector("#app > div.workspace-area > div:nth-child(3) > div:nth-child(2) > div > div > div:nth-child(2) > div > div > div > div > div > div.scrollable > div:nth-child(3)"));
+        searchInput = new WebDriverWait(driver,10)
+                .until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#app > div.workspace-area > div:nth-child(3) > div:nth-child(2) > div > div > div:nth-child(2) > div > div > div > div > div > div.scrollable > div:nth-child(3)")));
         searchInput.click();
         Thread.sleep(2000);
         driver.quit();
