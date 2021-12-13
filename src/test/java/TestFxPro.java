051010a201f7064850012adce1a661de26077dae
Ivan Valui <Wanjawaluj111@gmail.com>
2021-12-13 20:51:27 +0300

    Add Assert

--- src/test/java/TestFxPro.java
+++ src/test/java/TestFxPro.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -14,7 +15,8 @@ public class TestFxPro {
         System.setProperty("webdriver.chrome.driver","chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://edge.fxpro.com/");
-
+        String timeBeforeChange = "";
+        String timeAfterChange = "";
 
         WebElement searchInput = new WebDriverWait(driver,10)
                 .until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#container > div > div > div > form > div > input:nth-child(4)")));
@@ -30,6 +32,9 @@ public class TestFxPro {
         searchInput = new WebDriverWait(driver,10)
                 .until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#app > div.sidebar > section.sidebar__trailing > i.font-icon.icon_settings")));
         searchInput.click();
+
+        timeBeforeChange = driver.findElement(By.cssSelector("#app > div.workspace-area > div.workspace > div > div.layout-cell.horizontal > div:nth-child(3) > div.window.window-visible-overflow > div.tile.chart-tile.good.tile-window.widget-container > div > svg > g.axis.vertical > text:nth-child(7)")).getText();
+
         searchInput = new WebDriverWait(driver,10)
                 .until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#app > div.workspace-area > div:nth-child(3) > div > div > div > div:nth-child(2) > div > div > div > div > div > div:nth-child(1)")));
         searchInput.click();
@@ -37,13 +42,18 @@ public class TestFxPro {
                 .until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#app > div.workspace-area > div:nth-child(3) > div:nth-child(2) > div > div > div:nth-child(2) > div > div > div > div > div > div.scrollable > div:nth-child(1)")));
         searchInput.click();
 
+        timeAfterChange = driver.findElement(By.cssSelector("#app > div.workspace-area > div.workspace > div > div.layout-cell.horizontal > div:nth-child(3) > div.window.window-visible-overflow > div.tile.chart-tile.good.tile-window.widget-container > div > svg > g.axis.vertical > text:nth-child(7)")).getText();
+
         searchInput = new WebDriverWait(driver,10)
                 .until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#app > div.workspace-area > div:nth-child(3) > div > div > div > div:nth-child(2) > div > div > div > div > div > div:nth-child(1)")));
         searchInput.click();
         searchInput = new WebDriverWait(driver,10)
                 .until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#app > div.workspace-area > div:nth-child(3) > div:nth-child(2) > div > div > div:nth-child(2) > div > div > div > div > div > div.scrollable > div:nth-child(3)")));
         searchInput.click();
-        Thread.sleep(2000);
+
+        Assert.assertNotEquals(timeBeforeChange, timeAfterChange);
+
+        Thread.sleep(1000);
         driver.quit();
     }
 }
